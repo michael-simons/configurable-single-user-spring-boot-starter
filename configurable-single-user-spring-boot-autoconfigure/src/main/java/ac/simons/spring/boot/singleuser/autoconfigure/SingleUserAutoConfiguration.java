63f583a2b1331e7551e66ba6ad21db2ebe1ceec3
Michael J. Simons <michael@simons.ac>
2017-10-14 17:58:33 +0200

    Make sure this configuration is applied before Spring Security

--- configurable-single-user-spring-boot-autoconfigure/src/main/java/ac/simons/spring/boot/singleuser/autoconfigure/SingleUserAutoConfiguration.java
+++ configurable-single-user-spring-boot-autoconfigure/src/main/java/ac/simons/spring/boot/singleuser/autoconfigure/SingleUserAutoConfiguration.java
@@ -15,8 +15,10 @@
  */
 package ac.simons.spring.boot.singleuser.autoconfigure;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -36,6 +38,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  */
 @Configuration
 @ConditionalOnClass({AuthenticationManager.class, GlobalAuthenticationConfigurerAdapter.class})
+@AutoConfigureBefore(SecurityAutoConfiguration.class)
 public class SingleUserAutoConfiguration {
 
     @ConditionalOnMissingBean({AuthenticationManager.class, AuthenticationProvider.class, UserDetailsService.class})
