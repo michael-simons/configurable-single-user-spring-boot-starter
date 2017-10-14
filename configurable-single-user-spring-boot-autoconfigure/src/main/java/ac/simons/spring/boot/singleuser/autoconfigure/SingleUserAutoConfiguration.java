262da88b7cc7c8ad156aa21db5d753b34eb0a0c3
Michael J. Simons <michael@simons.ac>
2017-10-14 17:53:37 +0200

    Add first version of singleUserDetailsMananger

---
+++ configurable-single-user-spring-boot-autoconfigure/src/main/java/ac/simons/spring/boot/singleuser/autoconfigure/SingleUserAutoConfiguration.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 michael-simons.eu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ac.simons.spring.boot.singleuser.autoconfigure;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * Provides the auto-configuration of one single, user. This configuration is
+ * dependent on having Spring Security Core on the classpath (represented by the
+ * AuthenticatenManager) and also Spring Security Configuration (represented by
+ * the GlobalAuthenticationConfigurerAdapter).
+ *
+ * @author Michael J. Simons, 2017-10-14
+ */
+@Configuration
+@ConditionalOnClass({AuthenticationManager.class, GlobalAuthenticationConfigurerAdapter.class})
+public class SingleUserAutoConfiguration {
+
+    @ConditionalOnMissingBean({AuthenticationManager.class, AuthenticationProvider.class, UserDetailsService.class})
+    @Bean
+    public InMemoryUserDetailsManager singleUserDetailsMananger() throws Exception {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user").password("foo").roles().build());
+    }
+}
