405ae680d6fdabc39e0b04dc2eb5c439b3467109
Michael J. Simons <michael@simons.ac>
2017-10-18 20:00:20 +0200

    Add integration test to demo

---
+++ configurable-single-user-spring-boot-demo/src/test/java/ac/simons/spring/boot/singleuser/demo/ApplicationTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 michael-simons.eu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ac.simons.spring.boot.singleuser.demo;
+
+import java.net.URI;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import static org.springframework.http.RequestEntity.get;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Michael J. Simons, 2017-10-18
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"singleuser.name = peter", "singleuser.password = parker"})
+public class ApplicationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void helloShouldBeProtected() throws Exception {
+        final RequestEntity<Void> hello = get(URI.create("/hello")).accept(MediaType.APPLICATION_JSON).build();
+        assertThat(restTemplate.exchange(hello, String.class).getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        assertThat(restTemplate.withBasicAuth("user", "something").exchange(hello, String.class).getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        assertThat(restTemplate.withBasicAuth("peter", "parker").exchange(hello, String.class).getBody()).isEqualTo("Hello, peter\n");
+    }
+}
